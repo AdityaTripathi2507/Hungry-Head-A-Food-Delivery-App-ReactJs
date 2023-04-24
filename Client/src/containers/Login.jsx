e466b0310693a9f52ae293cbc7c7059e9e820ffc
AdityaTripathi2507 <adityatripathi2507@gmail.com>
2023-04-25 02:31:25 +0530

    started working on the server side

--- Client/src/containers/Login.jsx
+++ Client/src/containers/Login.jsx
@@ -4,12 +4,28 @@ import { LoginInput } from "../components";
 import { FaEnvelope, FaLock, FcGoogle } from "../assests/icons";
 import { motion } from "framer-motion";
 import { buttonClick } from "../animations";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { app } from "../config/firebase.config";
 
 const Login = () => {
   const [userEmail, setUserEmail] = useState("");
   const [isSignUp, setIsSignUp] = useState(false);
   const [password, setPassword] = useState("");
   const [confirm_password, setConfirm_password] = useState("");
+  const firebaseAuth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const loginWithGoogle = async () => {
+    await signInWithPopup(firebaseAuth, provider).then((userCred) => {
+      firebaseAuth.onAuthStateChanged((cred) => {
+        if (cred) {
+          cred.getIdToken().then((token) => {
+            console.log(token);
+          });
+        }
+      });
+    });
+  };
 
   return (
     <div className=" w-screen h-screen realtive overflow-hidden flex">
@@ -116,6 +132,7 @@ const Login = () => {
         <motion.div
           {...buttonClick}
           className="flex items-center justify-center px-20 py-2 bg-LightOverlay backdrop-blur-md cursor-pointer rounded-3xl gap-4"
+          onClick={loginWithGoogle}
         >
           <FcGoogle className="text-3xl" />
           <p className="capitalize text-base text-headingColor">
